313ee13838707b1f273204e3cc5745608fc7ca60
Sune Radich Vestergaard <srv@1508.dk>
2016-10-06 09:11:15 +0200

    Force use of strict mode

--- page_test.js
+++ page_test.js
@@ -1,32 +1,37 @@
+(function() {
 
-Feature('Testing pages');
+    "use strict";
 
-let server = require('./server');
+    Feature('Testing pages');
 
-AfterSuite((I) => {
-    server.close();
-});
+    let server = require('./server');
 
-BeforeSuite((I) => { // or Background
-    server.start();
-});
+    AfterSuite((I) => {
+        server.close();
+    });
 
-Scenario('Test for response 200', (I) => {
+    BeforeSuite((I) => { // or Background
+        server.start();
+    });
 
-    I.customAmOnPage('/page200');
-    I.getServerResponseCode(200);
+    Scenario('Test for response 200', (I) => {
 
-});
+        I.customAmOnPage('/page200');
+        I.getServerResponseCode(200);
 
-Scenario('Test for response 404', (I) => {
+    });
 
-    I.customAmOnPage('/page404');
-    I.getServerResponseCode(404);
+    Scenario('Test for response 404', (I) => {
 
-});
+        I.customAmOnPage('/page404');
+        I.getServerResponseCode(404);
 
-Scenario('Test for response 500', (I) => {
+    });
 
-    I.customAmOnPage('/page500');
-    I.getServerResponseCode(500);
-});
+    Scenario('Test for response 500', (I) => {
+
+        I.customAmOnPage('/page500');
+        I.getServerResponseCode(500);
+    });
+
+}());
