707caac946d2bdbd209afd95b23e12da8b619ddc
Sune Radich Vestergaard <srv@1508.dk>
2016-10-06 10:10:00 +0200

    Make it easier to add more test files
    Test files are now moved too its own folder

---
+++ test/page_responsecodes_test.js
@@ -0,0 +1,35 @@
+
+"use strict";
+
+Feature('Testing page response codes');
+
+//Require the mock Express server
+let server = require('./../server');
+
+AfterSuite((I) => {
+    server.close();
+});
+
+BeforeSuite((I) => { // or Background
+    server.start();
+});
+
+Scenario('Response code 200', (I) => {
+
+    I.customAmOnPage('/page200');
+    I.getServerResponseCode(200);
+
+});
+
+Scenario('Response code 404', (I) => {
+
+    I.customAmOnPage('/page404');
+    I.getServerResponseCode(404);
+
+});
+
+Scenario('Response code 500', (I) => {
+
+    I.customAmOnPage('/page500');
+    I.getServerResponseCode(500);
+});
