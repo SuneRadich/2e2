8f32aa745a9a1e89b9bee3f7b1e5026a9ba821ea
Sune Radich Vestergaard <srv@1508.dk>
2016-10-06 09:17:32 +0200

    Add better structure in the file

--- server.js
+++ server.js
@@ -1,33 +1,41 @@
+/**
+* Mock server for demo purposes only
+*/
+
 var express = require('express');
 var app = express();
+var server;
 
-var exports = module.exports = {};
-
+//Path to respond with regular response code
 app.get('/page200', function(req, res) {
     res.status(200);
     res.send('Page with response code 200');
 });
 
+
+//Response with 404
 app.get('/page404', function(req, res) {
     res.status(404);
     res.send('Err 404');
 });
 
+//Response with server error
 app.get('/page500', function(req, res) {
     res.status(500);
     res.send('Server error');
 });
 
-var server;
+//Expose functions
+module.exports = {
+    start: function start() {
+        server = app.listen(3000, function() {
+            console.log('Starting mock webserver on port 3000')
+        });
+    },
 
-exports.start = function() {
-    server = app.listen(3000, function() {
-        console.log('Starting mock webserver on port 3000')
-    });
-};
+    close: function close() {
+        console.log('Closing mock webserver')
+        server.close();
+    }
 
-
-exports.close = function() {
-    console.log('Closing mock webserver')
-    server.close();
 };
